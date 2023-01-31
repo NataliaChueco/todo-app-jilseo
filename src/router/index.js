3116cdc65a1a4fb1128c8a41f5cdd6c8b02be21a
NataliaChueco <natchueco@gmail.com>
2023-01-31 17:20:28 +0100

    env variables for netlify

--- src/router/index.js
+++ src/router/index.js
@@ -4,29 +4,36 @@ import Home from "../pages/Home.vue";
 import SignIn from "../pages/SignIn.vue";
 import SignUp from "../pages/SignUp.vue";
 
-const routes = [  {
-  path: "/",
-  redirect: "/signup"
-},
-{
-  path: "/signup",
-  name: "SignUp",
-  component: SignUp,
-},
-{
-  path: "/signin",
-  name: "SignIn",
-  component: SignIn,
-},
-{
-  path: "/dashboard",
-  name: "Home",
-  component: Home,
-}];
+const routes = [
+	{
+		path: "/",
+		redirect: "/signup",
+	},
+	{
+		path: "/signup",
+		name: "SignUp",
+		component: SignUp,
+	},
+	{
+		path: "/signin",
+		name: "SignIn",
+		component: SignIn,
+	},
+	{
+		path: "/dashboard",
+		name: "Home",
+		component: Home,
+	},
+];
 
-const router = createRouter({
+/* const router = createRouter({
   history: createWebHistory(import.meta.env.VITE_APP_ENV),
   routes,
 });
-
+ */
+const router = createRouter({
+	history: createWebHistory(import.meta.env.BASE_URL),
+	base: import.meta.env.BASE_URL,
+	routes,
+});
 export default router;
